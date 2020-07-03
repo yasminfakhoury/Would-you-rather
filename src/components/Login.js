0786d1f1a1afcc106fdaea04b8d4ac9feb9fa19e
Yasmin Fakhoury <yasminfakhoury@gmail.com>
2020-07-03 19:46:39 -0400

    fixed issue with useState

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import {_getUsers as getUsers} from '../utils/_DATA';
 
-// needs to be connected to store so that we can see if the username and 
-// password that were inputted match up with a user in the database
+// needs to be connected to store so that we can set the authedUser
 const Login = () => {
 
     // current state of the input values
     const [ userEntry, setUserEntry ] = useState('');
     const [ passwordEntry, setPasswordEntry ] = useState('');
+    const [ foundUser, setFoundUser ] = useState(false);
+
+    // once the foundUser slice of the state gets updated, we will either move on to the home page
+    // if the username and password match, otherwise we will display an error message
+    useEffect(() => {
+
+    })
+
 
     // check if username exists, and if it matches password
     async function checkUser() {
@@ -15,23 +23,22 @@ const Login = () => {
        // we're not going to dispatch an action because we are not modifying the store
        // instead, we check for the username and password here
         const users = await getUsers();
-        console.log(users);
         let found = false;
 
         // loop through the users to see if any match userEntry, then check is the associated
         // password matches
         Object.values(users).forEach(user => {
-            console.log(user.id);
             if(user.id === userEntry) {
                 if(user.password === passwordEntry) {
                     found = true;
-                    console.log('found it!');
                 }
             }
         });
 
-        if(found === false) console.log('username or password is incorrect')
-        else console.log('wahoo');
+        if(found === true) setFoundUser(true);
+        else setFoundUser(false);
+
+        console.log(`foundUser: ${foundUser}`)
 
     };
 
